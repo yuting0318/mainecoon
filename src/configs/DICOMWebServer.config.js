3b78964b90e4374129e77fd124633fbc4202fff5
yaokaixuan2000 <143184442+yaokaixuan2000@users.noreply.github.com>
2024-04-09 09:13:11 +0800

    切換dicomwebserver

--- src/configs/DICOMWebServer.config.js
+++ src/configs/DICOMWebServer.config.js
@@ -1,19 +1,42 @@
+// 連接自己的DICOMWebServer
+
+// module.exports = {
+//     // QIDO: {
+//     //     enableHTTPS: true,
+//     //     hostname: "ditto.dicom.tw",
+//     //     port: "",
+//     //     pathname: "/dicom-web",
+//     //     Token: null
+//     // },
+//     // WADO: {
+//     //     enableHTTPS: true,
+//     //     hostname: "ditto.dicom.tw",
+//     //     port: "",
+//     //     URI_pathname: "/dicom-web/wado",
+//     //     RS_pathname: "/dicom-web",
+//     //     Mode: "rs",
+//     //     Token: null
+//     // }
+// }\
+
+
+// 連接J4Care的DICOMWebServer
 module.exports = {
     QIDO: {
         enableHTTPS: true,
-        hostname: "ditto.dicom.tw",
-        // hostname:"",
-        port: "",
-        pathname: "/dicom-web",
-        Token: null
+        hostname: "development.j4care.com",
+        port: "11443",
+        pathname: "/dcm4chee-arc/aets/DCM4CHEE/rs",
+        Token: null,
     },
     WADO: {
         enableHTTPS: true,
-        hostname: "ditto.dicom.tw",
-        port: "",
-        URI_pathname: "/dicom-web/wado",
-        RS_pathname: "/dicom-web",
+        // hostname: "ditto.dicom.tw",
+        hostname: "development.j4care.com",
+        port: "11443",
+        URI_pathname: "/dcm4chee-arc/aets/DCM4CHEE/rs",
+        RS_pathname: "/dcm4chee-arc/aets/DCM4CHEE/rs",
         Mode: "rs",
-        Token: null
-    }
-}
+        Token: null,
+    },
+};
